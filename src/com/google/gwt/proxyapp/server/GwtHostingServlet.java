cc26c4272048922094d550e5dcb4d0e685d9291a
Ron Wright <ronald.wright71@gmail.com>
2015-04-02 15:47:32 -0700

    Completed program logic for GwtHostingServlet

--- src/com/google/gwt/proxyapp/server/GwtHostingServlet.java
+++ src/com/google/gwt/proxyapp/server/GwtHostingServlet.java
@@ -9,38 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.proxyapp.shared.FieldVerifier;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.CompositeFilter;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Entity;
 
-@SuppressWarnings("serial")
+@SuppressWarnings({ "serial", "unused" })
 public class GwtHostingServlet extends HttpServlet {
 	
 		public String getClient() {
-		return client;
+		return curClient;
 	}
 
 	public void setClient(String client) {
-		this.client = client;
+		this.curClient = client;
 	}
 
-		public String client = "GWT User";
+		public String curClient = "GWT User";
 	@Override
 	 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
@@ -55,49 +46,125 @@ public class GwtHostingServlet extends HttpServlet {
 	  }
 	 
 	public void doHosting(PrintWriter writer, HttpServletRequest req, HttpServletResponse resp) throws IOException {
-	    String clientName = req.getParameter("clientName");
 	    setClient(req.getParameter("clientName"));
-    	if (!FieldVerifier.isValidName(clientName)) clientName = "FreeWilly";
-		String addClient = req.getParameter("addClient"); 
+	    String clientName = getClient();
+    	if (!FieldVerifier.isValidName(clientName)) {clientName = "FreeWilly"; setClient(clientName);}
 		String ip = getClientIpAddress(req);
 		String user = req.getParameter("clientName") + "user";
 		Date date = new Date();
 		
 	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	    
 	    Key clientKey = KeyFactory.createKey("Client", clientName);
-	    // Run an ancestor query to ensure we see the most up-to-date
-	    // view of the Greetings belonging to the selected Guestbook.
+	    Key dclientKey = KeyFactory.createKey("Dclients", clientName);
+	    
+	    Query q;
+		List<Entity> dclients;
 		
+		String lddclient = ldclientList(datastore);
+	    
+//Testing db reset    	deleteClientRecords(datastore);
+		
+	    q = new Query("Dclients", dclientKey);
+	    dclients = datastore.prepare(q).asList(FetchOptions.Builder.withLimit(5));
+	    // Run an ancestor query to ensure we see the most up-to-date
+	    // view of the Clients belonging to Default Client List.
 	    Query query = new Query("Clients", clientKey).addSort("date", Query.SortDirection.DESCENDING);
 	    List<Entity> clients = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(5));
-
 	    if (clients.isEmpty()) {
-	    	if (FieldVerifier.isValidName(addClient) && addClient.matches("true")) {
-	    		clientName = "<b>Loading New Client to list of NX hosts....</b>";
-
-	    		Entity newclient = new Entity("Clients", clientKey);
-	            newclient.setProperty("user", user);
-	            newclient.setProperty("date", date);
-	            newclient.setProperty("ipaddress", ip);
-	            datastore.put(newclient);
-	            clientName = "</p<p>Client Name: &lt<b>" + client + "</b>&gt is now added to the list of valid Clients</p>";
+	    	
+	    	if (clients.isEmpty() && !dclients.isEmpty()) {
+	    		if (dclients.get(0).getProperty("updVal").equals(true)){
+	    			clientName = "<b>Loading New Client to list of NX hosts....</b>";
+	    			Entity newclient = new Entity("Clients", clientKey);
+	    			newclient.setProperty("user", user);
+	    			newclient.setProperty("date", date);
+	    			newclient.setProperty("ipaddress", ip);
+	    			datastore.put(newclient);
+	    			clientName = "</p<p>Client Name: &lt<b>" + curClient + "</b>&gt is now added to the list of valid Clients</p>";
+	    		}
 	    	} else {
-	    	//put clientName
-	    	clientName = "add client := " + addClient +" Client Name: &lt<b>" + clientName + "</b>&gt is not in the list of valid Clients</p>" +
-	    	"<p>Send an email to <a href=\"mailto:ronald.wright71@gmail.com\">ronald.wright71@gmail.com</a> for more information and/or to be added to the list";
+	    	//clientName not found
+	    	clientName = "Client List:=(" + lddclient + ") " + ip + " Client Name: &lt<b>" + curClient + 
+	    			"</b>&gt is not in the list of valid Clients</p>" +
+	    			"<p>Send an email to <a href=\"mailto:ronald.wright71@gmail.com\">ronald.wright71@gmail.com</a> " + 
+	    			"for more information and/or to be added to the list";
 	    	}
 	    } else {
-	    	//return results
-	    	clientName = "Client name found for: " + clientName;
+	    	
+	    	clientName = "Client name found for: " + curClient + "</p>" +  
+	    			"<p>Retrivieng information for Client</p>";
+	    	clientName = clientName + "<p>Comparing to previous</p>";	    	
+	    	//should update?	    
+	    	String clientlist = "";
+			for (Entity client : clients) {
+				String oldip= client.getProperty("ipaddress").toString();
+				if (!oldip.equals(ip)){
+	    			clientName = clientName + "<p>Updating ip Address from " + oldip + " to " + ip + "</p>";
+	    			client.setProperty("ipaddress", ip);
+					datastore.put(client);
+	    		} else {
+	    	        clientName = clientName + "<p>Records for Client " + curClient + " are as follows</p>";
+	        		clientlist = clientlist + "<p>" + client.getProperties().toString() + "</p>";
+//Testing  			client.setProperty("ipaddress", "192.168.0.29");
+//	    			datastore.put(client);
+	    		}
+	    	}
+
+    		clientName = clientName + clientlist;
 	    }
 		
 		writer.append("<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">")
 			   .append("<link type=\"text/css\" rel=\"stylesheet\" href=\"/ProxyApp.css\">")
 		       .append("<script type=\"text/javascript\" src=\"/proxyapp/proxyapp.nocache.js\"></script>")
-		       .append("</head><body><table align=\"center\" width=390px><tr><td><div><p><h1>Hello, " + client)
-		       .append("!</h1></p><p>" + ip + " " + clientName + "</p><p><form method=\"post\" action=\"/\">")
-		       .append("<input class=\".sendButton\" type=\"submit\" value=\"Return\"></input></form></p></td></tr></table></div></body></html>");
+		       .append("</head><body><table align=\"center\" width=390px><tr><td><div><p><h1>Hello, " + curClient)
+		       .append("!</h1></p><p>" + clientName + "</p><p><form method=\"post\" action=\"/\">")
+		       .append("<input class=\".sendButton\" type=\"submit\" value=\"Return\"></input></form></p></td></tr>" + 
+		    		   "</table></div></body></html>");
 	}
+
+	public String ldclientList(DatastoreService datastore) {
+		Query q = new Query("Dclients");
+	    List<Entity> dclients = datastore.prepare(q).asList(FetchOptions.Builder.withLimit(5));
+//	    Check to see if default client list has been loaded 
+	    if (dclients.isEmpty()){	   
+	    	//No Client List loading defaults
+			addDclients(datastore);
+	    }
+	    String lddclient = "";
+	    int cnt = 0;
+	    for (Entity dclient : dclients){
+	    	if (cnt == 0){lddclient = dclient.getKey().getName(); cnt++;} else
+	    	{lddclient = lddclient + ", " + dclient.getKey().getName();}
+	    	//Normally commented, for testing delete all Kinds of type Dclients 
+	    	//datastore.delete(dclient.getKey());	    	
+	    }
+		return lddclient;
+	}
+
+	public void deleteClientRecords(DatastoreService datastore) {
+		//delete all client records
+    	Query delclientRecs = new Query ("Clients");
+    	PreparedQuery delList = datastore.prepare(delclientRecs);
+		for (Entity delclientRec : delList.asIterable()){
+    		datastore.delete(delclientRec.getKey());	
+    	}
+	}
+	
+	private static final String[] DEFAULT_CLIENT_LIST = {
+		"roadrunner",
+		"CarlyJoe",
+		"Bubba",
+		"Crystal",
+		"Opie" };
+	
+	public static void addDclients(DatastoreService datastore){
+		for (String dclient : DEFAULT_CLIENT_LIST ){
+			Entity newclient = new Entity("Dclients", dclient);
+			newclient.setProperty("updVal", false);
+			datastore.put(newclient);
+		}
+	} 
 	
 	private static final String[] HEADERS_TO_TRY = { 
 	    "X-Forwarded-For",
